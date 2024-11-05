ee940f20350073426b158cb243c96a4b2d34faf6
zakitowa1210 <g2227077@st.t-kougei.ac.jp>
2024-11-05 14:41:11 +0900

    Stage
    序盤のステージを作った

--- MIZU/Assets/Zakitowa/Script/WallSwitch.cs
+++ MIZU/Assets/Zakitowa/Script/WallSwitch.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class WallSwitch : MonoBehaviour
 {
-    public GameObject wall;  // ????????????
+    public GameObject wall;  // 消したい壁のオブジェクト
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))  // ????????????????
+        if (other.CompareTag("Player"))  // プレイヤーがスイッチに触れたとき
         {
-            wall.SetActive(false);  // ????????
+            wall.SetActive(false);  // 壁を非表示にする
         }
     }
 }
